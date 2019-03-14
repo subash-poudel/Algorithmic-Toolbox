eff8ed95a67047a5dc84fe2c319c6db2925cd7eb
= <=>
2019-03-14 12:28:39 +0545

    Finish Week 3 Money change problem

--- src/com/subash/poudel/Main.java
+++ src/com/subash/poudel/Main.java
@@ -4,10 +4,11 @@ import com.subash.poudel.week2.FibonacciImproved;
 import com.subash.poudel.week2.Fibonnacci;
 import com.subash.poudel.week2.GreatestCommonDivisor;
 import com.subash.poudel.week2.LeastCommonMultiple;
+import com.subash.poudel.week3.Change;
 
 public class Main {
 
     public static void main(String[] args) {
-        FibonacciImproved.test();
+        Change.test();
     }
 }
---
+++ src/com/subash/poudel/week3/Change.java
@@ -0,0 +1,46 @@
+package com.subash.poudel.week3;
+
+import java.util.Scanner;
+
+public class Change {
+
+    private Change() {}
+
+    public static void test() {
+        String status = getChange(99) == 14 ? "Pass" : "Fail";
+        String status1 = getChange(28) == 6 ? "Pass" : "Fail";
+        System.out.println(status);
+        System.out.println(status1);
+    }
+
+
+//    Task. The goal in this problem is to find the minimum number of coins needed to change the input value
+//            (an integer) into coins with denominations 1, 5, and 10.
+//
+//    Input: 2
+//    Output: 2
+//
+//    Input: 28
+//    Output: 6
+    public static int getChange(int m) {
+        if(m < 0) {
+            // can throw a runtime exception
+            return -1;
+        }
+        if(m == 0) {
+            return m;
+        }
+        // should always be in ascending order
+        int[] denominations = {10, 5, 1};
+        int coins = 0;
+        for(int denomination: denominations) {
+            coins += m / denomination;
+            m = m % denomination;
+            if(m == 0) {
+                return coins;
+            }
+        }
+        return coins;
+    }
+}
+
